b6f93993c44a6785a12fc232a7804e3eb9b596d2
Maxwell Dantas <144301678+maxwell-dantas@users.noreply.github.com>
2025-11-14 17:53:53 -0300

    Lista ordenada

--- funcoes_recursivas/16.py
+++ funcoes_recursivas/16.py
@@ -10,10 +10,13 @@
 def junta(l1, l2):
     if (len(l1) == 0):
         return l2
-    if (len(l2) == 0):
+    if(len(l2) == 0 ):
         return l1
     
-    if (l1[-1] > l2[-1]):
-        return junta(l1[:-1], l2) + [l1[-1]]
+    maior_elemento_l1 = l1[-1]
+    maior_elemento_l2 = l2[-1]
+
+    if maior_elemento_l1 > maior_elemento_l2:
+        return junta(l1[:-1], l2) + [maior_elemento_l1]
     else:
-        return junta(l1, l2[:-1]) + [l2[-1]]
+        return junta(l1, l2[:-1]) + [maior_elemento_l2]
